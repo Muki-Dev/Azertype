217e7828347eb69a620e8072c1edac1a95567abd
Muki Dev <mukidev881@gmail.com>
2024-01-29 15:48:23 +0100

    ajout tableau liste de phrases

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 //  Déclarez une variable listeMots qui est un tableau
 const listeMots = ["Cachalot", "Pétunia" , "Serviette"];
 
+// Déclarez un tableau listePhrases
+const listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"];
+
 // Déclarez une deuxième variable score
 let score = 0;
 
+// Repeter le code avec une boucle
 for(let i = 0 ; i < listeMots.length ; i++){
     let motUtilisateur = prompt("Tapez les 3 mots de la listes: " + listeMots[i]);
     if(motUtilisateur === listeMots[i]){
