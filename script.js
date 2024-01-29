4ec9f85a711ca55e78feffdc28dc68f56bca6032
Muki Dev <mukidev881@gmail.com>
2024-01-29 14:48:27 +0100

    verification mot utilisateur

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const listeMots = ["Cachalot", "Pétunia" , "Serviette"];
 
 // Déclarez une deuxième variable score
 let score = 0;
+
+// À l’aide de l’instruction prompt, demandez à l’utilisateur de rentrer le mot contenu dans la première case du tableau. N’oubliez pas, la première case d’un tableau est la case numéro zéro !
+let motUtilisateur = prompt("Entrez le premier mot de la liste: " + listeMots[0]);
+if(motUtilisateur === listeMots[0]){
+    score += 1;
+    console.log(score);
+}
