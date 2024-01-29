0772f5d41c43b0fa38e025772734a2a2b06300f5
Muki Dev <mukidev881@gmail.com>
2024-01-29 15:01:34 +0100

    code plus propre

--- script.js
+++ script.js
@@ -4,31 +4,20 @@ const listeMots = ["Cachalot", "Pétunia" , "Serviette"];
 // Déclarez une deuxième variable score
 let score = 0;
 
-// À l’aide de l’instruction prompt, demandez à l’utilisateur de rentrer le mot contenu dans la première case du tableau. N’oubliez pas, la première case d’un tableau est la case numéro zéro !
-let motUtilisateur = prompt("Entrez le premier mot de la liste: " + listeMots[0]);
+
+let motUtilisateur = prompt("Entrez le mot: " + listeMots[0]);
 if(motUtilisateur === listeMots[0]){
-    score += 1;
-    console.log(score);
-}else{
-    console.log("Vous avez fait une erreur de frappe, ressayer enocre!")
+    score++
 }
 
-
-// À l’aide de l’instruction prompt, demandez à l’utilisateur de rentrer le mot contenu dans la première case du tableau. N’oubliez pas, la première case d’un tableau est la case numéro zéro !
- motUtilisateur = prompt("Entrez le premier mot de la liste: " + listeMots[1]);
+ motUtilisateur = prompt("Entrez mot: " + listeMots[1]);
 if(motUtilisateur === listeMots[1]){
-    score += 1;
-    console.log(score);
-}else{
-    console.log("Vous avez fait une erreur de frappe, ressayer enocre!")
+    score ++;
 }
 
-
-// À l’aide de l’instruction prompt, demandez à l’utilisateur de rentrer le mot contenu dans la première case du tableau. N’oubliez pas, la première case d’un tableau est la case numéro zéro !
- motUtilisateur = prompt("Entrez le premier mot de la liste: " + listeMots[2]);
+ motUtilisateur = prompt("Entrez mot: " + listeMots[2]);
 if(motUtilisateur === listeMots[2]){
-    score += 1;
-    console.log(score);
-}else{
-    console.log("Vous avez fait une erreur de frappe, ressayer enocre!")
+    score ++;
 }
+
+console.log(score);
