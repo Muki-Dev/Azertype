f4527791888296d08e42eec1c3ecc9d273cd7f30
Muki Dev <mukidev881@gmail.com>
2024-01-29 14:53:32 +0100

    test des trois mots

--- script.js
+++ script.js
@@ -12,3 +12,23 @@ if(motUtilisateur === listeMots[0]){
 }else{
     console.log("Vous avez fait une erreur de frappe, ressayer enocre!")
 }
+
+
+// À l’aide de l’instruction prompt, demandez à l’utilisateur de rentrer le mot contenu dans la première case du tableau. N’oubliez pas, la première case d’un tableau est la case numéro zéro !
+ motUtilisateur = prompt("Entrez le premier mot de la liste: " + listeMots[1]);
+if(motUtilisateur === listeMots[1]){
+    score += 1;
+    console.log(score);
+}else{
+    console.log("Vous avez fait une erreur de frappe, ressayer enocre!")
+}
+
+
+// À l’aide de l’instruction prompt, demandez à l’utilisateur de rentrer le mot contenu dans la première case du tableau. N’oubliez pas, la première case d’un tableau est la case numéro zéro !
+ motUtilisateur = prompt("Entrez le premier mot de la liste: " + listeMots[2]);
+if(motUtilisateur === listeMots[2]){
+    score += 1;
+    console.log(score);
+}else{
+    console.log("Vous avez fait une erreur de frappe, ressayer enocre!")
+}
