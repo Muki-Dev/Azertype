52255dcda94bd2f1025fb7db0be78ad2b29f2e13
Muki Dev <mukidev881@gmail.com>
2024-01-30 18:10:15 +0100

    selectionnez la zone texte propose et le score

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,7 @@ lancerJeu();
 let inputEcriture = document.getElementById("inputEcriture");
 
 let btnValiderMot = document.getElementById("btnValiderMot");
+
+let zoneProposition = document.querySelector(".zoneProposition");
+
+let zoneScore = document.querySelector(".zoneScore span");
