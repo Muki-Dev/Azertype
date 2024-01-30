fb1f007b523151c4417106040e7ae339760625e3
Muki Dev <mukidev881@gmail.com>
2024-01-30 13:03:31 +0100

    fonction principal lancerJeu et suppression de code répétitif

--- script.js
+++ script.js
@@ -4,34 +4,6 @@ const listeMots = ["Cachalot", "Pétunia" , "Serviette"];
 // Déclarez un tableau listePhrases
 const listePhrases = ["Pas de panique !", "La vie, l’univers et le reste", "Merci pour le poisson"];
 
-// Déclarez une deuxième variable score
-let score = 0;
-
-// Répétez la question tant que l’utilisateur n’a pas écrit “mots” ou “phrases”.
-let choix = prompt("tapez 'Mots' ou 'phrases'");
-while(choix !== "Mots" && choix !== "Phrases"){
-    choix = prompt("tapez 'Mots' ou 'phrases");
-}
-if(choix === "Mots"){
-    // Repeter le code avec une boucle
-    for(let i = 0 ; i < listeMots.length ; i++){
-        let motUtilisateur = prompt("Tapez le mot: " + listeMots[i]);
-        if(motUtilisateur === listeMots[i]){
-            score++;
-        }
-    }
-    console.log("votre score est: " + score + " sur " + listeMots.length);
-}else{
-    // Repeter le code avec une boucle
-    for(let i = 0 ; i < listePhrases.length ; i++){
-        let motUtilisateur = prompt("Tapez la phrase: " + listePhrases[i]);
-        if(motUtilisateur === listePhrases[i]){
-            score++;
-        }
-    }
-    console.log("votre score est: " + score + " sur " + listePhrases.length);
-}
-
 function afficherResultat(score,nbMotsProposes){
     // on affiche le score de l'utilisateur
     console.log("votre score est " + score + " sur " + nbMotsProposes);
@@ -56,4 +28,19 @@ function lancerBoucleDeJeu(listesPropositions){
         }
     }
     return score;
+}
+
+function lancerJeu(){
+    let choix = choisirPhrasesOuMots();
+    let score = 0;
+    let nbMotsProposes = 0;
+    if(choix === "mots"){
+        score = lancerBoucleDeJeu(listeMots);
+        nbMotsProposes = listeMots.length;
+    }else{
+        score = lancerBoucleDeJeu(listePhrases);
+        nbMotsProposes = listePhrases.length;
+    }
+
+    afficherResultat(score,nbMotsProposes)
 }
