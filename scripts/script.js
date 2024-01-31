e449a6496c61cda3c958c0f55a0b7843b79f28ea
Muki Dev <mukidev881@gmail.com>
2024-01-31 02:17:58 +0100

    affichez les mots que l'utilisateur doit recopier

--- scripts/script.js
+++ scripts/script.js
@@ -7,18 +7,31 @@ function afficherResultat(score,nbMotsProposes){
 
 
 
-
+function afficherProposition(proposition){
+    let zoneProposition = document.querySelector('.zoneProposition');
+    zoneProposition.innerHTML = proposition;   
+}
 
 function lancerJeu(){
     let score = 0;
     let nbMotsProposes = 0;
+    let i = 0;
 
     let zoneEcriture = document.getElementById('inputEcriture');
 
     let btnValiderMot = document.getElementById('btnValiderMot');
-    
+
+    afficherProposition(listeMots[i]);
     btnValiderMot.addEventListener('click',()=>{
-        console.log(zoneEcriture.value);
+        console.log(zoneEcriture.value)
+        i++;
+        inputEcriture.value = '';
+        if(listeMots[i] === undefined){
+            afficherProposition('Le jeu est fini');
+            btnValiderMot.disabled = true;
+        }else{
+            afficherProposition(listeMots[i]);
+        }
     })
     
     
