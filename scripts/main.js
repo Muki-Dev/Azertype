e77ef3fda6f4353ee7ff30b59fda740b582b4e24
Muki Dev <mukidev881@gmail.com>
2024-02-01 13:36:03 +0100

    sommettre formulaire

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,24 @@
-lancerJeu();
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
 
-let inputEcriture = document.getElementById("inputEcriture");
+lancerJeu()
 
-let btnValiderMot = document.getElementById("btnValiderMot");
+// J'ai mis ce code en commentaire, nous pourrons le récupérer lorsque nous en auront besoin :
 
-let zoneProposition = document.querySelector(".zoneProposition");
+// let inputEcriture = document.getElementById("inputEcriture")
+// console.log(inputEcriture)
 
-let zoneScore = document.querySelector(".zoneScore span");
+// let btnValiderMot = document.getElementById("btnValiderMot")
+// console.log(btnValiderMot)
 
-let listesBoutonRadio = document.querySelectorAll(".optionSource input");
+// let zoneProposition = document.querySelector(".zoneProposition")
+// console.log(zoneProposition)
+
+// let spanScore = document.querySelector(".zoneScore span")
+// console.log(spanScore)
+
+// let listeBtnRadio = document.querySelectorAll(".optionSource input")
+// console.log(listeBtnRadio)
