8f53f419e16748e3659d6e7263fe6f0e5f1af965
Muki Dev <mukidev881@gmail.com>
2024-02-01 10:30:16 +0100

    recuprer valeur input radio

--- scripts/script.js
+++ scripts/script.js
@@ -15,25 +15,41 @@ function afficherProposition(proposition){
 function lancerJeu(){
     let score = 0;
     let i = 0;
+    let listesProposition = listeMots;
 
     let zoneEcriture = document.getElementById('inputEcriture');
 
     let btnValiderMot = document.getElementById('btnValiderMot');
 
-    afficherProposition(listeMots[i]);
+    afficherProposition(listesProposition[i]);
+
     btnValiderMot.addEventListener('click',()=>{
-        if(zoneEcriture.value === listeMots[i]){
+        if(zoneEcriture.value === listesProposition[i]){
             score++
         }
         i++;
         afficherResultat(score,i);
         inputEcriture.value = '';
-        if(listeMots[i] === undefined){
+        if(listesProposition[i] === undefined){
             afficherProposition('Le jeu est fini');
             btnValiderMot.disabled = true;
         }else{
-            afficherProposition(listeMots[i]);
+            afficherProposition(listesProposition[i]);
         }
     })
+
+    let listeBtnRadio = document.querySelectorAll('.optionSource input');
+    
+    for(let index = 0; index < listeBtnRadio.length; index++){
+        listeBtnRadio[index].addEventListener('change',(event) =>{
+            console.log(event.target.value);
+            if(event.target.value === "1"){
+                listesProposition = listeMots;
+            }else{
+                listesProposition = listePhrases;
+            }
+            afficherProposition(listesProposition[i]);
+        })
+    }
     afficherResultat(score,i);
 }
